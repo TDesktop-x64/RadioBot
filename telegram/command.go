5ba891e4b8ec666e1603b66e60b73abd441b073a
c0re100 <corehusky@gmail.com>
2021-10-19 06:49:30 +0800

    Fix latest command

--- telegram/command.go
+++ telegram/command.go
@@ -94,7 +94,7 @@ func checkLatestSong(chatID, msgID int64, offset int) {
 		if songList[i] == nil {
 			continue
 		}
-		list += fmt.Sprintf("<b>%v</b>. <code>%v</code>\n", i+1, songList[i])
+		list += fmt.Sprintf("<b>%v</b>. <code>%v - %v</code>\n", i+1, songList[i].Artist, songList[i].Track)
 	}
 	format, err := bot.ParseTextEntities(list, tdlib.NewTextParseModeHTML())
 	if err != nil {
